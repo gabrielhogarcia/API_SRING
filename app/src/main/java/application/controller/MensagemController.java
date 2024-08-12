18a437a5bd9a291ee04e87c1b3c94b5176cab2e1
Igor Galvão <igorgalvao092@gmail.com>
2024-08-12 17:54:25 +0000

    .

--- app/src/main/java/application/controller/MensagemController.java
+++ app/src/main/java/application/controller/MensagemController.java
@@ -1,8 +1,19 @@
 package application.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MensagemController {
-    
+    private String[] frutas = {"Maçã", "Banana", "Morango"};
+
+    @GetMapping("/frutas")
+    public String[] list() {
+        return this.frutas;
+    }
+    @GetMapping("/frutas/{id}")
+    public String list (@PathVariable int id){
+        return this.frutas[id];
+    }
 }
